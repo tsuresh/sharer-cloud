61ba534b57977faa1f107967a131d88a09d4afed
Suresh Peiris <tsmpmail@gmail.com>
2022-03-06 01:22:24 +0530

    Completed workflow and docker

--- contributor_client/runtime/docker_env.py
+++ contributor_client/runtime/docker_env.py
@@ -1,49 +1,68 @@
 import docker
-from contributor_client.config import configs
-
-#chosen environments @TODO make it choosable
-environments = ['busybox']
 
 # Retrive docker status
 try:
     client = docker.from_env()
-    print(client.containers.list())
 except:
     print('Docker has not started or not running')
 
+containerLabel = "sharercloud-wl"
+
 # Pull the user prefered images
-def pullImages():
-    for environment in config.environments:
+def pullImages(configs: object):
+    for environment in configs.environments:
         if environment not in client.images.list():
             print("Fetching image..." + environment)
             client.images.pull(environment)
             print("Fetching image successful..." + environment)
+    return True
 
 # Create the container under the given system boundaries
-def create(name, image, deployCmd, envars):
-    container = client.containers.create(
-        name= name,
-        image= image,
-        command= deployCmd,
-        cpu_count= configs.cpu_count or 1,
-        cpu_percent= configs.cpu_percent or 100,
-        device_requests= configs.device_requests,
-        mem_limit= configs.mem_limit,
-        entrypoint= entryPoint,
-        environment= envars
-    )
-    print("Container created: " + container)
+def create(name: str, image: str, deployCmd: list, envars: list, configs: object):
+    try:
+        container = client.containers.create(
+            name= name,
+            image= image,
+            command= deployCmd,
+            cpu_count= configs.cpu_count or 1,
+            cpu_percent= configs.cpu_percent or 100,
+            #device_requests= configs.device_requests,
+            mem_limit= configs.mem_limit,
+            environment= envars,
+            labels= [containerLabel]
+        )
+        print("Container created")
+        print(container)
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
 # Starts the container and execute the initial commands
-def start(name, cmd):
-    container = client.containers.run(
-        name= name,
-        command= cmd,
-        auto_remove= True,
-    )
-    print("Container started: " + container)
+def start(name: str, cmd: list):
+    try:
+        container = client.containers.run(
+            name= name,
+            command= cmd,
+            auto_remove= True,
+        )
+        print("Container logs")
+        print(container)
+        return container
+    except:
+        print("Unable to start container")
+        return "Unable to start container"
 
-def startContainer():
-    print('start containers')
-
-initiateContainer('busybox')
+# Get a list of all running workloads
+def getRunningWorkloads():
+    try:
+        workloads = client.containers.list(
+            filters={
+                'label' : [containerLabel],
+                'status' : 'running'
+            }
+        )
+        return workloads
+    except:
+        print("Unable to get containers")
+        return []
--- contributor_client/workloads.py
+++ contributor_client/workloads.py
@@ -1,6 +1,134 @@
-#function to place workloads within running containers
+from queue import Empty
+import yaml
+import config
+import runtime.docker_env
 
+#place workloads within running containers
+def placeWorkloads(payloadId, payloadUrl, spec, config):
 
-#functions to get analytics
+    parsedSpec = parseSpec(spec)
 
+    #create a container and deploy the workload
+    containerStatus = runtime.docker_env.create(
+        "sharercloud-wl-" + payloadId,
+        parsedSpec["image"],
+        ["mkdir sc_wl_" + payloadId, "cd sc_wl_" + payloadId, "wget " + payloadUrl, "unzip " + str(payloadId) + ".zip"],
+        parsedSpec["envars"],
+        config
+    )
+
+    #start deploy commands
+    if containerStatus is True:
+        resultLog = runtime.docker_env.start(
+            "sharercloud-wl-" + payloadId,
+            parsedSpec["start_commands"]
+        )
+        print("Got the final result" + resultLog)
+    else:
+        print("Unable to create the container")
+
+    #pass result to frontend
+
+def checkPlaceability(spec, config):
+
+    parsedSpec = parseSpec(spec)
+
+    #Check subscribed images
+    if parsedSpec["image"] not in config.images:
+        print("Incompatible image")
+        return {
+            'status' : False,
+            'message' : 'Incompatible image'
+        }
+
+    #Check if there's existing processes
+    if len(runtime.docker_env.getRunningWorkloads()) > 0:
+        print("There are existing processes running")
+        return {
+            'status' : False,
+            'message' : 'There are existing processes running'
+        }
+
+    #Check if the spec matches the hardware
+    if parsedSpec["cpu_count"] is not None:
+        if parsedSpec["cpu_count"] > config.cpu_count:
+            print("Required CPU count is not sufficient")
+            return {
+                'status' : False,
+                'message' : 'Required CPU count is not sufficient'
+            }
+
+    #Check for CPU percentage allocation
+    if parsedSpec["cpu_percent"] is not None:
+        if parsedSpec["cpu_percent"] > config.cpu_percent:
+            print("Required CPU percentage is not sufficient")
+            return {
+                'status' : False,
+                'message' : 'Required CPU percentage is not sufficient'
+            }
+
+    #Check for special requests
+    if all(item in config.device_requests for item in parsedSpec["device_requests"]) is False:
+        print("Required special features are not available in device")
+        return {
+            'status' : False,
+            'message' : 'Required special features are not available in device'
+        }
+    
+    return {
+        'status' : True,
+        'message' : 'Container is ready to be scheduled'
+    }
+
+def parseSpec(parsedSpec):
+    spec = {}
+
+    if parsedSpec["spec"]["image"] is not None:
+        spec["image"] = parsedSpec["spec"]["image"]
+
+    if parsedSpec["spec"]["cpu_count"] is not None:
+        spec["cpu_count"] = parsedSpec["spec"]["cpu_count"]
+
+    if parsedSpec["spec"]["cpu_percent"] is not None:
+        spec["cpu_percent"] = parsedSpec["spec"]["cpu_percent"]
+
+    if parsedSpec["spec"]["device_requests"] is not []:
+        spec["device_requests"] = parsedSpec["spec"]["device_requests"]
+
+    if parsedSpec["spec"]["resultType"] is not None:
+        spec["resultType"] = parsedSpec["spec"]["resultType"] #rawOut, fileWrite, noOut
+
+    if parsedSpec["spec"]["start_commands"] is not []:
+        spec["start_commands"] = parsedSpec["spec"]["start_commands"]
+
+    if parsedSpec["spec"]["envars"] is not []:
+        spec["envars"] = []
+        for envar in parsedSpec["spec"]["envars"]:
+            envarKey = list(envar.keys())[0]
+            envarVals = list(envar.values())[0]
+            spec["envars"].append(envarKey + "=" + envarVals);
+
+    return spec
+
+def getYamlConfig():
+    with open("spec.yaml", "r") as stream:
+        try:
+            yamldata = yaml.safe_load(stream)
+            return yamldata
+        except yaml.YAMLError as exc:
+            print(exc)
+
+placeability = checkPlaceability(
+    getYamlConfig(),
+    config.configs
+)
+
+print(placeability)
+
+placement = placeWorkloads(
+    "123456",
+    "https://inforwaves.com/",
+    getYamlConfig(),
+    config.configs
+)
 
